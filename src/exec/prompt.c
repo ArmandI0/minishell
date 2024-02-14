67dfd5e8c4d519018e9815fcce88ac664d3063d5
Nicolas Ledent <nledent@1A2.42angouleme.fr>
2024-02-14 16:14:39 +0100

    set up readline

--- src/exec/prompt.c
+++ src/exec/prompt.c
@@ -1,23 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nledent <nledent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 15:19:19 by aranger           #+#    #+#             */
-/*   Updated: 2024/02/14 16:04:35 by nledent          ###   ########.fr       */
+/*   Updated: 2024/02/14 16:13:11 by nledent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int main(int argc, char **argv, char **envp)
+int	prompt_rl(void)
 {
-	(void)envp;
-	(void)argc;
-	(void)argv;
-	prompt_rl();
-	//list_of_command(argc, argv);
-	return (0);
+	char	*line;
+
+	line = readline("minishell>");
+	while (1)
+	{
+		add_history(line);
+		//send line to parsing here
+		if (line != NULL)
+			free(line);
+		line = readline("minishell>");
+	}
+	if (line != NULL)
+		free(line);
 }
