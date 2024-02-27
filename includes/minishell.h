acbdbbcf2b27c423bdbf81643a82e7dbececa468
aranger <aanger3@gmail.com>
2024-02-27 13:59:44 +0100

    avant merge prompt

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aranger <aranger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 12:20:46 by aranger           #+#    #+#             */
-/*   Updated: 2024/02/27 13:47:53 by aranger          ###   ########.fr       */
+/*   Updated: 2024/02/27 13:58:05 by aranger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@
 # include <errno.h>
 # include <signal.h>
 # include "../lib/libft/src/libft.h"
-#define TRUE 1
-#define FALSE 0
+# define TRUE 1
+# define FALSE 0
+
+typedef int t_bool;
 
 /* typedef */
 typedef	enum mn_errors
@@ -32,11 +34,7 @@ typedef	enum mn_errors
 	ER_NO_ARG,
 }			t_errors;
 
-typedef int t_bool;
-
-/* TOKEN for lexer*/
-
-typedef enum e_token
+typedef	enum token
 {
 	CHARACTER,
 	SINGLE_QUOTE,
@@ -44,17 +42,10 @@ typedef enum e_token
 	REDIRECTION_OPERATOR,
 	PIPE,
 	DOLLAR,
-	IS_SPACE,
-	END,
-} t_token;
+	SPACES,
+}			t_token;
 
-typedef struct s_lexer
-{
-	char *entry;
-	t_token	*lexing;
-}				t_lexer;
-
-typedef enum e_builtin
+typedef	enum builtin
 {
 	BT_NO,
 	BT_ECHO,
@@ -64,32 +55,27 @@ typedef enum e_builtin
 	BT_UNSET,
 	BT_ENV,
 	BT_EXIT,
-} t_builtin;
+}			t_builtin;
 
 /* s_env_var liste pour stocker variables d'environnement */
 /* exported : 0 = oui ; 1 = non */
-
 typedef struct s_env_var
 {
-	char *name;
-	char *value;
-	t_bool exported;
-	struct s_env_var *next;
-}				t_env_var;
+	char				*name;
+	char				*value;
+	int					exported;
+	struct s_env_var	*next;
+}			t_env_var;
+
 
 /* s_command contains all options and arguments for exec */
-
 typedef struct s_command
 {
-	char *name;
-	char *path;
-	char *command;
-}				t_cmd;
-
-/* s_redir contains all redirections for one command in the right order */
-/* in_out : 0 = in ; 1 = out - */
-/* app_mod_: 0 = oui ; 1 = non */
-/* bien initialiser chaque pointeur */
+	char	*name;
+	char	*path;
+	char	**args;
+	int		argc;
+}			t_cmd;
 
 /* s_redir contains all redirections for one command in the right order */
 /* in_out : 0 = in ; 1 = out - */
@@ -101,7 +87,6 @@ typedef struct s_redir
 	int				in_out;
 	int				app_mod_hdoc;
 	char			*lim_hdoc;
-	struct s_redir	*prev;
 	struct s_redir	*next;
 }			t_redir;
 
@@ -130,18 +115,7 @@ typedef struct s_shell_data
 }			t_sh_data;
 
 /* PARSING FUNCTIONS */
-
-t_lexer *lexing(char *line);
-char	**split_lexer(t_lexer *lx, t_token sep, char c);
-void		quote_error(t_lexer *lx);
-void	free_lexer(t_lexer *lx);
-
-/* test parsing*/
-t_bloc_cmd	*set_new_node(char *line, t_token *tline, char **envp);
-void	parsing(char *line, char **envp);
-t_cmd	*init_command_struct(char *arg, t_token *t_arg, char **envp);
-char	*find_command_path(char **envp, char *command);
-t_bool	check_path_acces(char *path);
+void	list_of_command(int argc, char **argv);
 
 /* BUILTINS FUNCTIONS */
 void	bt_echo(t_cmd *echo_cmd);
@@ -163,7 +137,7 @@ void 	free_env_var(t_env_var *var1);
 void	free_list_cmd(t_bloc_cmd *cmd_data);
 
 /* PROMPT TERMINAL FUNCTIONS */
-int		prompt_rl(void);
+int		prompt_rl(t_sh_data *sh_data);
 
 /* SIGNALS FUNCTIONS */
 void	init_signals(void);
