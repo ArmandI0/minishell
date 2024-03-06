9e9327c535c74f02876d2b2141c769256d36c180
aranger <aanger3@gmail.com>
2024-03-06 16:46:07 +0100

    ok

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: aranger <aranger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 16:57:46 by aranger           #+#    #+#             */
-/*   Updated: 2024/03/05 18:39:09 by aranger          ###   ########.fr       */
+/*   Updated: 2024/03/06 15:17:59 by aranger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -134,7 +134,7 @@ t_bool	parsing(char *line, t_sh_data *data)
 	printList(*a);
 	redirection_parsing(a, data);
 	command_parsing(a, data);
-	//print_all_bloc(data);
+	print_all_bloc(data);
 	ft_lstclear(a);
 	free(a);
 	free_lexer(lx);
