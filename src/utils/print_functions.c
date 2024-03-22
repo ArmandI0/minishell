1b533db932a3a1489134cf223fdfee7838a0f79c
aranger <aanger3@gmail.com>
2024-03-22 15:13:32 +0100

    ps youhou

--- src/utils/print_functions.c
+++ src/utils/print_functions.c
@@ -3,25 +3,81 @@
 /*                                                        :::      ::::::::   */
 /*   print_functions.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nledent <nledent@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aranger <aranger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 17:35:03 by nledent           #+#    #+#             */
-/*   Updated: 2024/03/20 17:07:09 by nledent          ###   ########.fr       */
+/*   Updated: 2024/03/22 15:11:03 by aranger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+static t_env_var	*duplicate_env(t_env_var *list)
+{
+	t_env_var	*tmp;
+	t_env_var	*start;
+
+	if (list)
+	{
+		tmp = ft_calloc(1 ,sizeof(t_env_var));
+		if (tmp == NULL)
+			return (NULL);
+		start = tmp;
+		while(list != NULL && tmp != NULL)
+		{
+			tmp->name = list->name;
+			tmp->value = list->value;
+			if (list->next != NULL)
+				tmp->next = ft_calloc(1 ,sizeof(t_env_var));
+			else
+				tmp->next = NULL;
+			tmp = tmp->next;
+			list = list->next;
+		}
+	}
+	return (start);
+}
+
+static t_env_var	*find_first(t_env_var *list)
+{
+	t_env_var	*target;
+	int			*a;
+
+	a = 0;
+	while (list->next != NULL)
+	{
+		if (ft_strcmp(list->name, list->next->name) < 0 && list->exported == 0)
+			target = list->name;
+		if (list->exported == 0)
+			a = 1;	
+	}
+	
+}
+
 void	print_env_export(t_sh_data *sh_data)
 {
-	t_env_var	*next;
+	t_env_var	*var;
+	t_env_var	*tmp;
+	t_env_var	*target;
+	int 		a;
 
-	next = sh_data->env_var1;
-	while (next != NULL)
+	tmp = duplicate_env(sh_data->env_var1);
+	while (1)
 	{
 		ft_printf_fd(1, "declare -x ");
-		ft_printf_fd(1, "%s=%s\n", next->name, next->value);
-		next = next->next;
+		var = tmp;
+		target = var;
+		a = 0;
+		while (var != NULL && var->exported != NULL)
+		{
+
+			if (var->exported == 0);
+				a == 1;
+			var = var->next;
+		}
+		target->exported = 0;
+		ft_printf_fd(1, "%s=%s\n", tmp->name, tmp->value);
+		tmp = tmp->next;
 	}
 }
 
